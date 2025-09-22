e46e789834abf0f66faa0dc08dbdee7e717eec00
Roofle Dev <developer@roofle.com>
2025-09-22 16:20:41 -0500

    stack overflow solution to use common js

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["@my-company/utils"],
+  webpack: (config, { isServer }) => {
+    config.resolve.alias["@my-company/utils"] = path.resolve(
+      __dirname,
+      "../utility-package/dist"
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
